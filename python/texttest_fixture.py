2b85349a386dfbabb28360c11c9c0db9f5eff83d
Emily Bache <emily.bache@pagero.com>
2016-10-03 15:33:39 +0200

    added python version of same kata

---
+++ python/texttest_fixture.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+from gilded_rose import *
+import csv
+
+if __name__ == "__main__":
+    print ("OMGHAI!")
+    items = []
+    with open("items.csv") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            items.append(Item(row['Name'], row['sellIn'], row['quality']))
+
+    days = 1
+    import sys
+    if len(sys.argv) > 1:
+        days = int(sys.argv[1]) + 1
+    for day in range(days):
+        print("-------- day %s --------" % day)
+        print("name, sellIn, quality")
+        for item in items:
+            print(item)
+        print("")
+        GildedRose(items).update_quality()
